ceaca9ff417918edec6bcc8ceee23d1b2c6a3aa1
Nikita Sharma <sniks12034@gmail.com>
2025-12-28 21:00:34 +0530

    Time: 612 ms (61.12%) | Memory: 0B (100.00%) - LeetSync

---
+++ LeetCode/2087-confirmation-rate/confirmation-rate.sql
@@ -0,0 +1,4 @@
+select s.user_id, round(ifnull(sum(c.action = 'confirmed')/count(c.user_id),0),2) as confirmation_rate 
+from signups as s
+left join confirmations as c on s.user_id = c.user_id
+group by s.user_id
