082aa271aecec5d99ee844ccfa57075a52a1bf7a
Nikita Sharma <sniks12034@gmail.com>
2025-12-29 14:46:35 +0530

    Time: 1139 ms (6.70%) | Memory: 0B (100.00%) - LeetSync

--- LeetCode/1182-game-play-analysis-iv/game-play-analysis-iv.sql
+++ LeetCode/1182-game-play-analysis-iv/game-play-analysis-iv.sql
@@ -1,24 +1,12 @@
-WITH PlayerActivity AS (
-    SELECT 
-        player_id,
-        event_date,
-        LEAD(event_date) OVER (
-            PARTITION BY player_id 
-            ORDER BY event_date
-        ) AS next_date,
-        MIN(event_date) OVER (
-            PARTITION BY player_id
-        ) AS first_date
-    FROM Activity
-),
-NextDayPlayers AS (
-    SELECT DISTINCT player_id
-    FROM PlayerActivity
-    WHERE next_date = DATE_ADD(first_date, INTERVAL 1 DAY)
-)
-SELECT 
-    ROUND(
-        COUNT(*) / (SELECT COUNT(DISTINCT player_id) FROM Activity),
-        2
-    ) AS fraction
-FROM NextDayPlayers;
+WITH first_login AS (SELECT player_id,
+MIN(event_date) AS first_date
+FROM Activity
+GROUP BY player_id),
+
+next_day_login AS (SELECT f.player_id
+FROM first_login f
+JOIN Activity a ON f.player_id = a.player_id AND a.event_date = DATE_ADD(f.first_date, INTERVAL 1 DAY))
+
+SELECT ROUND(COUNT(DISTINCT n.player_id) / COUNT(DISTINCT f.player_id), 2) AS fraction
+FROM first_login f
+LEFT JOIN next_day_login n ON f.player_id = n.player_id;
