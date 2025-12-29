93c438815717abe4239fae9bb5c674e25f836599
Nikita Sharma <sniks12034@gmail.com>
2025-12-29 13:39:08 +0530

    Time: 642 ms (89.73%) | Memory: 0B (100.00%) - LeetSync

---
+++ LeetCode/1155-product-sales-analysis-iii/product-sales-analysis-iii.sql
@@ -0,0 +1,12 @@
+WITH first_sales AS (
+    SELECT 
+        product_id, 
+        MIN(year) AS first_year
+    FROM Sales
+    GROUP BY product_id
+)
+SELECT s.product_id,f.first_year, s.quantity,s.price
+FROM Sales AS s
+JOIN first_sales AS f
+  ON s.product_id = f.product_id
+ AND s.year = f.first_year;
