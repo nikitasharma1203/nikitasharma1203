4b99f08b5cbc1bfc72c0b8d7a33bd5f86b06f31e
Nikita Sharma <sniks12034@gmail.com>
2025-12-30 16:59:58 +0530

    Time: 1093 ms (78.56%) | Memory: 0B (100.00%) - LeetSync

---
+++ LeetCode/1268-market-analysis-i/market-analysis-i.sql
@@ -0,0 +1,5 @@
+SELECT u.user_id as buyer_id, u.join_date, COUNT(o.order_id) AS orders_in_2019
+FROM Users as u
+LEFT JOIN Orders as o ON u.user_id = o.buyer_id
+AND YEAR(o.order_date) = 2019
+GROUP BY 1,2
