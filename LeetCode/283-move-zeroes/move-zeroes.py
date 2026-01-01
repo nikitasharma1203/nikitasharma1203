b55ebf4db2cc434183fc8df50fc5be50504580a0
Nikita Sharma <sniks12034@gmail.com>
2026-01-01 15:35:36 +0530

    Time: 8 ms (25.96%) | Memory: 18.5 MB (89.84%) - LeetSync

---
+++ LeetCode/283-move-zeroes/move-zeroes.py
@@ -0,0 +1,11 @@
+class Solution:
+    def moveZeroes(self, nums: List[int]) -> None:
+        """
+        Do not return anything, modify nums in-place instead.
+        """
+        next_non_zero = 0
+
+        for i in range(len(nums)):
+            if nums[i] != 0:
+                nums[next_non_zero], nums[i] = nums[i] , nums[next_non_zero]
+                next_non_zero +=1
