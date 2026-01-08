8d921cb1b6a93da8c348abdd2cd13fece37754bd
Nikita Sharma <sniks12034@gmail.com>
2026-01-08 16:58:22 +0530

    Time: 0 ms (100.00%) | Memory: 19.5 MB (14.50%) - LeetSync

--- LeetCode/1319-unique-number-of-occurrences/unique-number-of-occurrences.py
+++ LeetCode/1319-unique-number-of-occurrences/unique-number-of-occurrences.py
@@ -1,5 +1,4 @@
 class Solution:
     def uniqueOccurrences(self, arr: List[int]) -> bool:
-        freq = Counter(arr)
-        counts = list(freq.values())
-        return len(set(counts)) == len(counts)
+        count = Counter(arr)
+        return len(set(count.values())) == len(count)
