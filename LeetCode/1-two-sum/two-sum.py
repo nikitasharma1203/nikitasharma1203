05a943a8fa86069e3775f461150133242da43046
Nikita Sharma <sniks12034@gmail.com>
2026-01-17 18:12:46 +0530

    Time: 0 ms (100.00%) | Memory: 20.6 MB (8.54%) - LeetSync

--- LeetCode/1-two-sum/two-sum.py
+++ LeetCode/1-two-sum/two-sum.py
@@ -7,3 +7,11 @@ class Solution:
                 return [seen[complement], i]
             else:
                 seen[num] = i
+"""class Solution:
+    def twoSum(self, nums, target):
+        n = len(nums)
+        for i in range(n):
+            for j in range(i+1, n):
+                if nums[i] + nums[j] == target:
+                    return [i, j]
+"""
