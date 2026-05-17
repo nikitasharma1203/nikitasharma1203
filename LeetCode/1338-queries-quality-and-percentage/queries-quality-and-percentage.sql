726b6efc36d3434762d3c5c22c203e781d7376b4
Nikita Sharma <sniks12034@gmail.com>
2026-05-17 16:56:02 +0530

    Time: 372 ms (44.71%) | Memory: 0B (100.00%) - LeetSync

--- LeetCode/1338-queries-quality-and-percentage/queries-quality-and-percentage.sql
+++ LeetCode/1338-queries-quality-and-percentage/queries-quality-and-percentage.sql
@@ -1,4 +1,6 @@
-select query_name , round(avg(rating*1/position),2) as quality, 
-round(sum(case when rating < 3 then 1 else 0 end) * 100 / count(*),2) as poor_query_percentage
-from queries
-group by query_name
+SELECT 
+    query_name,
+    ROUND(AVG(rating * 1.0 / position), 2) AS quality,
+    ROUND(100.0 * AVG(CASE WHEN rating < 3 THEN 1 ELSE 0 END), 2) AS poor_query_percentage
+FROM queries
+GROUP BY query_name;
