fcfecb5904525e6cfb0bce41c9bec9b8966a3425
Nikita Sharma <sniks12034@gmail.com>
2026-05-17 12:07:44 +0530

    Time: 238 ms (80.52%) | Memory: 0B (100.00%) - LeetSync

--- LeetCode/1801-average-time-of-process-per-machine/average-time-of-process-per-machine.sql
+++ LeetCode/1801-average-time-of-process-per-machine/average-time-of-process-per-machine.sql
@@ -1,9 +1,5 @@
-select a.machine_id,
-       round(sum(b.timestamp - a.timestamp) / count(a.process_id),3) as processing_time
-from Activity a
-join
-Activity b
-on a.machine_id = b.machine_id
-where a.activity_type = "start"
-and b.activity_type = "end"
+select a.machine_id, round(sum(b.timestamp - a.timestamp)/count(a.process_id),3) as processing_time
+from Activity as a
+join Activity as b on a.machine_id = b.machine_id 
+where a.activity_type = 'start' and b.activity_type = 'end'
 group by 1
