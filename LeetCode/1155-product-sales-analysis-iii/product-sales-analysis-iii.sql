952a6bc40b886bcd7f8a0d8ccbc4dafb9ed1b3ad
Nikita Sharma <sniks12034@gmail.com>
2026-05-23 18:20:04 +0530

    Time: 732 ms (43.28%) | Memory: 0B (100.00%) - LeetSync

--- LeetCode/1155-product-sales-analysis-iii/product-sales-analysis-iii.sql
+++ LeetCode/1155-product-sales-analysis-iii/product-sales-analysis-iii.sql
@@ -1,12 +1,13 @@
-WITH first_sales AS (
-    SELECT 
-        product_id, 
-        MIN(year) AS first_year
+SELECT 
+    s.product_id,
+    f.first_year,
+    s.quantity,
+    s.price
+FROM Sales AS s
+JOIN (
+    SELECT product_id, MIN(year) AS first_year
     FROM Sales
     GROUP BY product_id
-)
-SELECT s.product_id,f.first_year, s.quantity,s.price
-FROM Sales AS s
-JOIN first_sales AS f
+) AS f
   ON s.product_id = f.product_id
  AND s.year = f.first_year;
