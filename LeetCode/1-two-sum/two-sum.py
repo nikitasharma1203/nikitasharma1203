3af3074b7bc2afc9b86b2b5ef774e031255bb366
Nikita Sharma <sniks12034@gmail.com>
2026-05-26 23:02:45 +0530

    Time: 1733 ms (19.53%) | Memory: 19.8 MB (74.46%) - LeetSync

--- LeetCode/1-two-sum/two-sum.py
+++ LeetCode/1-two-sum/two-sum.py
@@ -1,10 +1,7 @@
-"""hashmap(o(n))"""
 class Solution:
     def twoSum(self, nums, target):
-        seen = {}
-        for i, j in enumerate(nums):
-            complement = target - j
-            if complement in seen:
-                return [seen[complement], i]
-            else:
-                seen[j] = i
+        n = len(nums)
+        for i in range(n):
+            for j in range(i+1, n):
+                if nums[i] + nums[j] == target:
+                    return [i, j]
