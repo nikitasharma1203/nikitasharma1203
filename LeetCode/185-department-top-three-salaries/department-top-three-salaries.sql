b388766a688b94043d8ccf0edf7c75520ff309c1
Nikita Sharma <sniks12034@gmail.com>
2026-05-28 20:45:47 +0530

    Time: 1502 ms (6.76%) | Memory: 0B (100.00%) - LeetSync

--- LeetCode/185-department-top-three-salaries/department-top-three-salaries.sql
+++ LeetCode/185-department-top-three-salaries/department-top-three-salaries.sql
@@ -1,11 +1,13 @@
-WITH ranked AS (
-SELECT e.id, e.name AS Employee, e.salary, e.departmentId,
-DENSE_RANK() OVER (PARTITION BY e.departmentId 
-ORDER BY e.salary DESC
-) AS rnk
-FROM Employee e)
-
-SELECT d.name AS Department, r.Employee, r.salary
-FROM ranked as r
-JOIN Department as d ON r.departmentId = d.id
-WHERE r.rnk <= 3;
+SELECT d.name AS Department,
+       e.name AS Employee,
+       e.salary AS Salary
+FROM Employee e
+JOIN Department d
+  ON e.departmentId = d.id
+WHERE (
+    SELECT COUNT(DISTINCT e2.salary)
+    FROM Employee e2
+    WHERE e2.departmentId = e.departmentId
+      AND e2.salary > e.salary
+) < 3
+ORDER BY Department, Salary DESC;
