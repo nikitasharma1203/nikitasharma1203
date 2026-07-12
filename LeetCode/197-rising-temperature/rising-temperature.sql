c77ca554c39cc51cbedcfa536e8b68a285ca8645
Nikita Sharma <sniks12034@gmail.com>
2026-07-12 09:45:00 +0530

    Time: 206 ms (99.91%) | Memory: 0B (100.00%) - LeetSync

--- LeetCode/197-rising-temperature/rising-temperature.sql
+++ LeetCode/197-rising-temperature/rising-temperature.sql
@@ -1,4 +1,5 @@
-select w1.id
+-- Write your PostgreSQL query statement below
+select w1.id 
 from weather as w1
-join weather as w2 on datediff(w1.recordDate, w2.recordDate) = 1
-where w1.temperature > w2.temperature
+join weather as w2 on w1.recordDate = w2.recordDate +1
+where w1.temperature > w2.temperature 
