d8c5998f4d9e1f3c8f8b0dd992d6c54e73d9ee97
Nikita Sharma <sniks12034@gmail.com>
2026-07-20 20:40:06 +0530

    Time: 731 ms (54.39%) | Memory: 0B (100.00%) - LeetSync

--- LeetCode/1155-product-sales-analysis-iii/product-sales-analysis-iii.sql
+++ LeetCode/1155-product-sales-analysis-iii/product-sales-analysis-iii.sql
@@ -1,8 +1,5 @@
-SELECT s.product_id, t.first_year, s.quantity, s.price
-FROM Sales s
-JOIN (
-    SELECT product_id, MIN(year) AS first_year
-    FROM Sales
-    GROUP BY product_id
-) t
-ON s.product_id = t.product_id AND s.year = t.first_year;
+select product_id, year as first_year, quantity, price
+from (select product_id, year, quantity, price,
+rank() over (partition by product_id order by year asc) as ranks from sales)
+as window_func
+where ranks = 1
