6709709eabeddffc721f11c263bbd15f31a7f7c9
Nikita Sharma <sniks12034@gmail.com>
2026-07-21 21:32:51 +0530

    Time: 681 ms (87.09%) | Memory: 0B (100.00%) - LeetSync

--- LeetCode/1155-product-sales-analysis-iii/product-sales-analysis-iii.sql
+++ LeetCode/1155-product-sales-analysis-iii/product-sales-analysis-iii.sql
@@ -1,5 +1,4 @@
 select product_id, year as first_year, quantity, price
 from (select product_id, year, quantity, price,
-rank() over (partition by product_id order by year asc) as ranks from sales)
-as window_func
-where ranks = 1
+rank() over (partition by product_id order by year) as rnk from sales) as wind
+where rnk =1
