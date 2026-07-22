c9bc99bdb645301fac3c88a3ef381952c136be56
Nikita Sharma <sniks12034@gmail.com>
2026-07-22 07:55:15 +0530

    Time: 1003 ms (52.91%) | Memory: 0B (100.00%) - LeetSync

--- LeetCode/1773-percentage-of-users-attended-a-contest/percentage-of-users-attended-a-contest.sql
+++ LeetCode/1773-percentage-of-users-attended-a-contest/percentage-of-users-attended-a-contest.sql
@@ -1,6 +1,4 @@
-SELECT 
-    r.contest_id,
-    ROUND(COUNT(DISTINCT r.user_id) * 100.0 / (SELECT COUNT(*) FROM Users), 2) AS percentage
-FROM Register r
-GROUP BY r.contest_id
-ORDER BY percentage DESC, r.contest_id ASC;
+select r.contest_id, round(count(distinct r.user_id)*100/(select count(user_id) from users),2) as percentage
+from users as u join register as r on u.user_id = r.user_id
+group by 1
+order by 2 desc, 1 asc
