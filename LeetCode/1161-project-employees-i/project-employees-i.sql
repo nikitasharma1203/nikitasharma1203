c3b909eed358a49dad86cca585a1e193b6a1ef4a
Nikita Sharma <sniks12034@gmail.com>
2026-07-22 07:49:53 +0530

    Time: 488 ms (92.46%) | Memory: 0B (100.00%) - LeetSync

--- LeetCode/1161-project-employees-i/project-employees-i.sql
+++ LeetCode/1161-project-employees-i/project-employees-i.sql
@@ -1,4 +1,3 @@
--- Write your PostgreSQL query statement below
-select project.project_id, round(sum(employee.experience_years)*1.0/count(project.employee_id),2) as average_years
-from project join employee on project.employee_id = employee.employee_id
+select project_id, round(avg(experience_years),2) as average_years
+from project as p join employee as e on p.employee_id = e.employee_id
 group by 1
