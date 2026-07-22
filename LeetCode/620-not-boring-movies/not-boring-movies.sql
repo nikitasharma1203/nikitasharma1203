6a38799d533d31f304ee89dd8aba22444ef0897a
Nikita Sharma <sniks12034@gmail.com>
2026-07-22 07:37:49 +0530

    Time: 283 ms (35.53%) | Memory: 0B (100.00%) - LeetSync

--- LeetCode/620-not-boring-movies/not-boring-movies.sql
+++ LeetCode/620-not-boring-movies/not-boring-movies.sql
@@ -1,5 +1,4 @@
--- Write your PostgreSQL query statement below
 select id, movie, description, rating
-from cinema
-where description != 'boring' and id%2 = 1
+from cinema 
+where description != 'boring' and id%2!=0
 order by 4 desc
