30aa411481475c04b81ececa0efd4bc851abbc0d
Nikita Sharma <sniks12034@gmail.com>
2026-07-22 07:47:23 +0530

    Time: 945 ms (23.68%) | Memory: 0B (100.00%) - LeetSync

--- LeetCode/1390-average-selling-price/average-selling-price.sql
+++ LeetCode/1390-average-selling-price/average-selling-price.sql
@@ -1,5 +1,4 @@
 select p.product_id, ifnull(round(sum(u.units*p.price)/sum(u.units),2),0) as average_price
-from prices as p 
-left join unitssold as u on u.product_id = p.product_id and u.purchase_date between p.start_date and p.end_date
-
+from prices as p left join unitssold as u on p.product_id = u.product_id
+and u.purchase_date between p.start_date and p.end_date
 group by 1
