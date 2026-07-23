6a61fa74b01cdf68b067237d803bbd6bfd08a39e
Nikita Sharma <sniks12034@gmail.com>
2026-07-23 15:38:52 +0530

    Time: 636 ms (6.03%) | Memory: 0B (100.00%) - LeetSync

--- LeetCode/1509-replace-employee-id-with-the-unique-identifier/replace-employee-id-with-the-unique-identifier.sql
+++ LeetCode/1509-replace-employee-id-with-the-unique-identifier/replace-employee-id-with-the-unique-identifier.sql
@@ -1,3 +1,3 @@
-select eu.unique_id, e.name
-from employees as e left join EmployeeUNI as eu
-on e.id = eu.id
+select unique_id, name
+from employees as e
+left join employeeuni as eu on eu.id = e.id
