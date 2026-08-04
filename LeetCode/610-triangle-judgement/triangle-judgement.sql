6a5a5eb9a0dca13ebca20c9c6fd0e82392f51447
Nikita Sharma <sniks12034@gmail.com>
2026-08-04 14:57:51 +0530

    Time: 357 ms (15.59%) | Memory: 0B (100.00%) - LeetSync

--- LeetCode/610-triangle-judgement/triangle-judgement.sql
+++ LeetCode/610-triangle-judgement/triangle-judgement.sql
@@ -1,9 +1,4 @@
-SELECT x, y, z,
-CASE 
-WHEN x + y > z 
-AND x + z > y 
-AND y + z > x 
-THEN 'Yes'
-ELSE 'No'
-END AS triangle
-FROM triangle;
+SELECT 
+    x, y, z,
+    IF(x + y > z AND x + z > y AND y + z > x, 'Yes', 'No') AS triangle
+FROM Triangle;
