47b416bfbfb599bfa6f21900ba2ca3cc2b40bac1
Nikita Sharma <sniks12034@gmail.com>
2026-08-09 09:40:29 +0530

    Time: 0 ms (100.00%) | Memory: 19.4 MB (38.46%) - LeetSync

--- LeetCode/88-merge-sorted-array/merge-sorted-array.py
+++ LeetCode/88-merge-sorted-array/merge-sorted-array.py
@@ -1,6 +1,7 @@
 class Solution:
-    def merge(self, nums1, m, nums2, n):
-        # Copy nums2 into the empty slots of nums1
+    def merge(self, nums1: List[int], m: int, nums2: List[int], n: int) -> None:
+        """
+        Do not return anything, modify nums1 in-place instead.
+        """
         nums1[m:] = nums2
-        # Sort the combined array
         nums1.sort()
