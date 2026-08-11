93249106d0b5a606d2caa38adc6b16d547650e8b
Nikita Sharma <sniks12034@gmail.com>
2026-08-11 19:02:13 +0530

    Time: 369 ms (71.83%) | Memory: 0B (100.00%) - LeetSync

--- LeetCode/1338-queries-quality-and-percentage/queries-quality-and-percentage.sql
+++ LeetCode/1338-queries-quality-and-percentage/queries-quality-and-percentage.sql
@@ -1,3 +1,3 @@
-select query_name, round(avg(rating/position),2) as quality,round(count(case when rating<3 then 1 end)*100/ count(*),2) as poor_query_percentage
+select query_name, round(avg(rating/position),2) as quality, round(count(case when rating < 3 then 1 end)*100/count(query_name),2) as poor_query_percentage
 from queries
 group by 1
