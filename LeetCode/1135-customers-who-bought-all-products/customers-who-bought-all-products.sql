6a59b6484b724880c1638d77d1d089c4b24fbe18
Nikita Sharma <sniks12034@gmail.com>
2026-08-12 13:58:51 +0530

    Time: 862 ms (7.54%) | Memory: 0B (100.00%) - LeetSync

--- LeetCode/1135-customers-who-bought-all-products/customers-who-bought-all-products.sql
+++ LeetCode/1135-customers-who-bought-all-products/customers-who-bought-all-products.sql
@@ -1,5 +1,4 @@
 select c.customer_id 
-from customer as c 
-join product as p on c.product_key = p.product_key
+from customer as c join product as p on c.product_key = p.product_key
 group by 1
 having count(distinct c.product_key) = (select count(product_key) from product)
