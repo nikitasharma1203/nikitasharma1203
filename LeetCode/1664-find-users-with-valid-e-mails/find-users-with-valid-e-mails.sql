1c177361002ecaa4d62c5311ce2c0683df29372f
Nikita Sharma <sniks12034@gmail.com>
2026-08-12 15:37:31 +0530

    Time: 1093 ms (9.93%) | Memory: 0B (100.00%) - LeetSync

--- LeetCode/1664-find-users-with-valid-e-mails/find-users-with-valid-e-mails.sql
+++ LeetCode/1664-find-users-with-valid-e-mails/find-users-with-valid-e-mails.sql
@@ -1,3 +1,3 @@
-SELECT user_id, name, mail
-FROM Users
-WHERE REGEXP_LIKE(mail, '^[A-Za-z][A-Za-z0-9_.-]*@leetcode[.]com$', 'c');
+select user_id, name, mail
+from users
+where regexp_like(mail,'^[A-Za-z][A-Za-z0-9_.-]*@leetcode[.]com$','c')
