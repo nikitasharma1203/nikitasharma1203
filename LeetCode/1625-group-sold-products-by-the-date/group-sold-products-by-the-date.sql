91f9a874dc37628363a838dda89b4f3d2c20f910
Nikita Sharma <sniks12034@gmail.com>
2026-08-12 15:44:24 +0530

    Time: 711 ms (6.59%) | Memory: 0B (100.00%) - LeetSync

--- LeetCode/1625-group-sold-products-by-the-date/group-sold-products-by-the-date.sql
+++ LeetCode/1625-group-sold-products-by-the-date/group-sold-products-by-the-date.sql
@@ -1,4 +1,4 @@
-select sell_date, count(distinct product) as num_sold, group_concat(distinct product order by product asc) as products
+select sell_date, count(distinct product) as num_sold, group_concat(distinct product order by product) as products
 from activities
-group by sell_date
-order by sell_date
+group by 1
+order by 1
