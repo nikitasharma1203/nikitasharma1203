460559fb1016ae5363e2d7a60f89e98d4ed57f64
Nikita Sharma <sniks12034@gmail.com>
2026-08-14 16:11:29 +0530

    Time: 894 ms (12.34%) | Memory: 0B (100.00%) - LeetSync

--- LeetCode/1882-the-number-of-employees-which-report-to-each-employee/the-number-of-employees-which-report-to-each-employee.sql
+++ LeetCode/1882-the-number-of-employees-which-report-to-each-employee/the-number-of-employees-which-report-to-each-employee.sql
@@ -1,6 +1,6 @@
-select e2.employee_id, e2.name, count(distinct e1.employee_id) as reports_count, round(avg(e1.age)) as average_age 
-from employees as e1
+select e2.employee_id, e2.name, count(distinct e1.employee_id) as reports_count, round(avg(e1.age)) as average_age
+from employees as e1 
 join employees as e2 on e1.reports_to = e2.employee_id
 group by 1,2
-having count(*) >=1
+having count(e1.employee_id) >=1
 order by 1
