0724222406a03cedbe59805feb12d1cba71cf729
Nikita Sharma <sniks12034@gmail.com>
2026-08-14 15:30:58 +0530

    Time: 951 ms (11.31%) | Memory: 0B (100.00%) - LeetSync

--- LeetCode/1292-immediate-food-delivery-ii/immediate-food-delivery-ii.sql
+++ LeetCode/1292-immediate-food-delivery-ii/immediate-food-delivery-ii.sql
@@ -1,3 +1,4 @@
-select round(count(case when order_date = customer_pref_delivery_date then 1 end)*100.0/ count(customer_id),  2) as immediate_percentage
-from (select customer_id, min(order_date) as order_date, min(customer_pref_delivery_date) as customer_pref_delivery_date from delivery
-group by customer_id) as wind
+select round(count(case when order_date = customer_pref_delivery_date then 1 end)*100/count(customer_id),2) as immediate_percentage
+from (select customer_id, min(order_date) as order_date, min(customer_pref_delivery_date) as customer_pref_delivery_date
+from delivery
+group by customer_id) as temp
