31efd226092f99b478fff9f69dd49745e5ebdbe5
Nikita Sharma <sniks12034@gmail.com>
2026-08-21 15:53:03 +0530

    Time: 260 ms (42.18%) | Memory: 0B (100.00%) - LeetSync

--- LeetCode/197-rising-temperature/rising-temperature.sql
+++ LeetCode/197-rising-temperature/rising-temperature.sql
@@ -1,4 +1,4 @@
-select w1.id
-from weather as w1
+select w1.id as id
+from weather as w1 
 join weather as w2 on w1.recorddate = w2.recorddate + 1
-where w1.temperature > w2.temperature
+where w1.temperature> w2.temperature
