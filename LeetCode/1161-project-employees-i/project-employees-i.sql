8cb501c9813a806b3274975a9100a1079d7bb277
Nikita Sharma <sniks12034@gmail.com>
2026-08-29 18:01:45 +0530

    Time: 519 ms (80.08%) | Memory: 0B (100.00%) - LeetSync

--- LeetCode/1161-project-employees-i/project-employees-i.sql
+++ LeetCode/1161-project-employees-i/project-employees-i.sql
@@ -1,3 +1,3 @@
-select project_id, round(avg(e.experience_years),2) as average_years
+select p.project_id, round(avg(e.experience_years),2) as average_years
 from project as p join employee as e on p.employee_id = e.employee_id
 group by 1
